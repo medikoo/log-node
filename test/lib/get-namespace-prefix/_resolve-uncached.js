66aa11830b6a1e201af3f2cb81836bb7c0f81671
Mariusz Nowak <medyk@medikoo.com>
2019-03-19 10:56:12 +0100

    test: refactor into multiple tests

---
+++ test/lib/get-namespace-prefix/_resolve-uncached.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const requireUncached = require("cjs-module/require-uncached")
+    , overrideEnv     = require("process-utils/override-env");
+
+module.exports = callback => {
+	const { restoreEnv } = overrideEnv();
+	try {
+		return requireUncached(
+			[
+				require.resolve("log"), require.resolve("log/lib/private/logger-prototype"),
+				require.resolve("log/lib/emitter"),
+				require.resolve("log/lib/get-default-namespace"),
+				require.resolve("../../../lib/get-namespace-prefix"),
+				require.resolve("supports-color"),
+				require.resolve("../../../lib/private/colors-support-level")
+			],
+			() => {
+				callback();
+				return {
+					log: require("log"),
+					getNamespacePrefix: require("../../../lib/get-namespace-prefix")
+				};
+			}
+		);
+	} finally {
+		restoreEnv();
+	}
+};
